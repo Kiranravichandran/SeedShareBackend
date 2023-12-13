ec0791275706e116a607cacdd4822ead61c678ed
Kiran R <kiran.r@presidio.com>
2023-12-13 12:56:24 +0530

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545", // URL of your local Geth node
+      chainId: 1, // Chain ID of your local network (must match Geth's networkid)
+      allowUnlimitedContractSize: true,
+    },
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
