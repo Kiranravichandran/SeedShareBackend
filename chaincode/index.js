76a3368e14cbe9387c5e712a22218640fd790ad6
Kiran R <kiran.r@presidio.com>
2023-12-16 08:41:52 +0530

    Dev: Added bin files

--- chaincode/index.js
+++ chaincode/index.js
@@ -3,4 +3,4 @@
 const adminContract = require('./admin_contract');
 const FarmerContract = require('./farmer_contract')
 
-module.exports.contracts = [adminContract,FarmerContract];
+module.exports.contracts = [FarmerContract, adminContract];
--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
 export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/pee rs/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/ Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_LOCALMSPID="Org1MSP"
 cd test-network
 sh ./network.sh up
 sh ./network.sh createChannel
 sh ./network.sh deployCC -ccn SeedshareNetwork -ccl javascript -ccp ../chaincode -ccv 1 -ccs 1 -cci instantiate
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls true --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n SeedshareNetwork --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"function":"requestNewFarmer","Args":["Kiran","kiran.r@presidio.com","1234567890","123456789012"]}'
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls true --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n SeedshareNetwork --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"function":"viewUser","Args":["Kiran","123456789012"]}'
