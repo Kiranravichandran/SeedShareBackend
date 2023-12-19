affab63cdfc30613cf50f1fdd1aff8b611b000f6
Kiran R <kiran.r@presidio.com>
2023-12-19 10:16:23 +0530

    Added seddshare module

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
+from service.SeedshareModule import SeedshareService
 with open("constants/app-response.json") as f:
     output = json.load(f)
 
@@ -10,6 +11,19 @@ def healthCheck():
     output["status_code"] = 200
     output["response"] = "Hello from Seedshare"
     return jsonify(output)
+
+@app.route("/createFarmer", methods=["POST"])
+def create_farmer():
+    try:
+        request_data = request.data
+        request_data_str = request_data.decode("utf-8")
+        json_data = request.get_json()
+        result = SeedshareService.createFarmer(json_data["name"],json_data["emailId"], json_data["phoneNumber"], json_data["aadharNumber"])
+        output["status_code"] = 200
+        output["response"] = result
+        return jsonify(output)
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == "__main__":
     app.run(port=8080,debug=True)
